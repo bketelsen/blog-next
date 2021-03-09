8638d9027bb79e21d0f21fa1981ad97b91f8f4e3
Brian Ketelsen <bketelsen@gmail.com>
2021-03-09 12:41:13 -0500

    checkpoint : saving all data types

--- scripts/data.mjs
+++ scripts/data.mjs
@@ -33,11 +33,20 @@ fs.readFile("data/writers.json", function (err, data) {
   // Converting to JSON 
   const writers = JSON.parse(data);
   writers.forEach(writer => {
-    getData(writer.picture.url, `./public${writer.picture.url}`)
+    saveWriter(writer, mdxConfig)
   })
 
 });
+fs.readFile("data/categories.json", function (err, data) {
+  // Check for errors 
+  if (err) throw err;
+  // Converting to JSON 
+  const categories = JSON.parse(data);
+  categories.forEach(category => {
+    saveCategory(category, mdxConfig)
+  })
 
+});
 fs.readFile("data/global.json", function (err, data) {
   // Check for errors 
   if (err) throw err;
@@ -50,13 +59,11 @@ fs.readFile("data/global.json", function (err, data) {
 function saveImage(image, config) {
   const imageConfig = config["image"];
   const fileName = path.join(imageConfig.contentPath, image.hash + ".mdx")
-  console.log(Object.keys(image.formats))
 
   getData(image.url, `./public${image.url}`)
 
   Object.keys(image.formats).forEach(function (i) {
     if (i === 'base64') {
-      console.log(image.formats[i])
       const b6 = {
         url: image.formats[i].url,
         width: image.formats[i].width,
@@ -118,5 +125,52 @@ ${article.content}
   saveImage(article.image, config)
   saveImage(article.seo.shareImage, config)
 }
+function saveWriter(writer, config) {
+  console.log("converting writer", writer.slug)
+
+  const contents = (writer) => `
+---
+name: ${yaml.dump(writer.name).trimEnd()}
+bio: ${yaml.dump(writer.bio).trimEnd()}
+email: ${yaml.dump(writer.email).trimEnd()}
+twitter: ${yaml.dump(writer.twitter).trimEnd()}
+youtube: ${yaml.dump(writer.youtube).trimEnd()}
+github: ${yaml.dump(writer.github).trimEnd()}
+linkedin: ${yaml.dump(writer.linkedin).trimEnd()}
+jobtitle: ${yaml.dump(writer.jobtitle).trimEnd()}
+company: ${yaml.dump(writer.company).trimEnd()}
+picture: 
+  ${yaml.dump([writer.picture.hash]).trimEnd()}
+avatar: 
+  ${yaml.dump([writer.avatar.hash]).trimEnd()}
+---`
+  const writerConfig = config["author"];
+  const fileName = path.join(writerConfig.contentPath, writer.slug + ".mdx")
+  // create the target directory if it doesn't exist 
+  if (!fs.existsSync(writerConfig.contentPath)) {
+    fs.mkdirSync(articleConfig.contentPath);
+  }
+  // write the contents
+  fs.writeFileSync(fileName, contents(writer).trim());
+  saveImage(writer.picture, config)
+  saveImage(writer.avatar, config)
+}
 
 
+function saveCategory(category, config) {
+  console.log("converting category", category.slug)
+
+  const contents = (category) => `
+---
+name: ${yaml.dump(category.name).trimEnd()}
+plural: ${yaml.dump(category.plural).trimEnd()}
+---`
+  const categoryConfig = config["category"];
+  const fileName = path.join(categoryConfig.contentPath, category.slug + ".mdx")
+  // create the target directory if it doesn't exist 
+  if (!fs.existsSync(categoryConfig.contentPath)) {
+    fs.mkdirSync(categoryConfig.contentPath);
+  }
+  // write the contents
+  fs.writeFileSync(fileName, contents(category).trim());
+}
